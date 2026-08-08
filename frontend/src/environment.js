5af5e4053b66e229270a0010b0f19a69bcd26eaa
Pankaj Singh <pankajsaini71004@gmail.com>
2026-08-08 19:18:12 +0530

    Update configuration for production

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 const server = process.env.NODE_ENV === "production" ?
-    "https://YOUR_RENDER_BACKEND_URL.onrender.com" :
+    "https://bharatmeet-app.onrender.com" :
     "http://localhost:8000"
 
 export default server;
